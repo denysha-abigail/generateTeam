9d5ad8b7de2d51f2e53157b4a11ef9be7728ab4e
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-05-01 19:00:14 -0400

    css styling

--- src/page-template.js
+++ src/page-template.js
@@ -1,16 +1,16 @@
 const generateTeam = team => {
 
     const generateManager = (manager) => {
-      return ` <div class="card" >
-          <div class="card-header">
-              <h2 class="card-title text-capitalize">${manager.getName()}</h2>
-              <h3 class="card-title"><span class="oi oi-briefcase mr-2"></span>${manager.getRole()}</h3>   
+      return ` <div class="card manager-card" >
+          <div class="card-header manager-header">
+              <h2 class="card-title text-capitalize text-center font-weight-bold manager-title">${manager.getName()}</h2>
+              <h3 class="card-title text-center font-weight-bold manager-title"><span class="oi oi-briefcase mr-2"></span>${manager.getRole()}</h3>   
           </div>
           <div class="card-body">
               <ul class="list-group">
-                  <li class="list-group-item">ID: ${manager.getId()}</li>
-                  <li class="list-group-item">EMAIL: <a href="mailto:${manager.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${manager.getEmail()}</a></li>
-                  <li class="list-group-item">OFFICE NUMBER: ${manager.getOfficeNumber()}</li>
+                  <li class="list-group-item manager-list">ID: ${manager.getId()}</li>
+                  <li class="list-group-item manager-list">EMAIL: <a class="text-uppercase" href="mailto:${manager.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${manager.getEmail()}</a></li>
+                  <li class="list-group-item manager-list">OFFICE NUMBER: ${manager.getOfficeNumber()}</li>
               </ul>
           </div>
           </div>   `;
@@ -20,16 +20,16 @@ const generateTeam = team => {
       if(!engineer){
         return '';
       }
-      return ` <div class="card" >
-          <div class="card-header">
-              <h2 class="card-title text-capitalize">${engineer.getName()}</h2>
-              <h3 class="card-title"><span class="oi oi-beaker mr-2"></span>${engineer.getRole()}</h3>   
+      return ` <div class="card engineer-card" >
+          <div class="card-header engineer-header">
+              <h2 class="card-title text-capitalize text-center font-weight-bold engineer-title">${engineer.getName()}</h2>
+              <h3 class="card-title text-center font-weight-bold engineer-title"><span class="oi oi-beaker mr-2"></span>${engineer.getRole()}</h3>   
           </div>
           <div class="card-body">
               <ul class="list-group">
-                  <li class="list-group-item">ID: ${engineer.getId()}</li>
-                  <li class="list-group-item">EMAIL: <a href="mailto:${engineer.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${engineer.getEmail()}</a></li>
-                  <li class="list-group-item">GITHUB: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+                  <li class="list-group-item engineer-list">ID: ${engineer.getId()}</li>
+                  <li class="list-group-item engineer-list">EMAIL: <a class="text-uppercase" href="mailto:${engineer.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${engineer.getEmail()}</a></li>
+                  <li class="list-group-item engineer-list">GITHUB: <a class="text-uppercase" href="https://github.com/${engineer.getGithub()}" target="_blank"><span class="oi oi-fork"></span>${engineer.getGithub()}</a></li>
               </ul>
           </div>
           </div>   `;
@@ -39,16 +39,16 @@ const generateTeam = team => {
       if(!intern){
         return '';
       }
-      return ` <div class="card" >
-      <div class="card-header">
-          <h2 class="card-title text-capitalize">${intern.getName()}</h2>
-          <h3 class="card-title"><span class="oi oi-calculator mr-2"></span>${intern.getRole()}</h3>   
+      return ` <div class="card intern-card" >
+      <div class="card-header intern-header">
+          <h2 class="card-title text-capitalize text-center font-weight-bold intern-title">${intern.getName()}</h2>
+          <h3 class="card-title text-center font-weight-bold intern-title"><span class="oi oi-calculator mr-2"></span>${intern.getRole()}</h3>   
       </div>
       <div class="card-body">
           <ul class="list-group">
-              <li class="list-group-item">ID: ${intern.getId()}</li>
-              <li class="list-group-item">EMAIL: <a href="mailto:${intern.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${intern.getEmail()}</a></li>
-              <li class="list-group-item text-capitalize">SCHOOL: ${intern.getSchool()}</li>
+              <li class="list-group-item intern-list">ID: ${intern.getId()}</li>
+              <li class="list-group-item intern-list">EMAIL: <a class="text-uppercase" href="mailto:${intern.getEmail()}"><span class="oi oi-envelope-closed mr-1"></span>${intern.getEmail()}</a></li>
+              <li class="list-group-item text-uppercase intern-list">SCHOOL: ${intern.getSchool()}</li>
           </ul>
       </div>
       </div>   `;
@@ -88,7 +88,6 @@ const generateTeam = team => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"/>
         <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="team.css">
       </head>
       
@@ -96,7 +95,7 @@ const generateTeam = team => {
         <div class="container my-5">
           <div class="row">
               <div class="col-12 jumbotron mb-3">
-                  <h1 class="txt-center">My Team<span class="oi oi-people ml-3"></span></h1>
+                  <h1 class="text-center font-weight-bold">My Team<span class="oi oi-people ml-3"></span></h1>
               </div>
           </div>
         </div>
